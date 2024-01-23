731ffce67875307df72b9431341aa3c4ab9b7618
Tim <tim.smith2814@gmail.com>
2024-01-23 14:56:48 +0000

    Change array for sql

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,13 @@
-export default function Home() {
-  const cats = [
-    { name: "Penguin", seeds: "poppy", height: "2ft", cute: true },
-    { name: "Chester", seeds: "sesame", height: "8000ft", cute: true },
-    { name: "Cooper", seeds: "sycamore", height: "0.4mm", cute: true },
-    { name: "Cheeni", seeds: "beet (sugar)", height: "-3ft", cute: true },
-    { name: "Tibbles", seeds: "flax", height: "8001ft", cute: true },
-    { name: "Jez", seeds: "woe", height: "5ft12in", cute: false },
-  ];
+import { sql } from "@vercel/postgres";
+
+export default async function Home() {
+  const cats = await sql`SELECT * FROM sunflowercats`;
 
   return (
     <div>
       <h2>Home</h2>
       <div className="cats">
-        {cats.map((cat) => {
+        {cats.rows.map((cat) => {
           return (
             <div key={cat.name} className="cat">
               <h3>{cat.name}</h3>
